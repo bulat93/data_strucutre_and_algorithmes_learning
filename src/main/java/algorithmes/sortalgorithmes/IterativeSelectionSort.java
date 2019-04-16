03b4c027128b01b84fd98fe71c63a19b078b846c
16687815 <Khusnutdinov.B.Ra@sberbank.ru>
2019-04-16 18:58:17 +0300

    update

--- src/main/java/algorithmes/sortalgorithmes/IterativeSelectionSort.java
+++ src/main/java/algorithmes/sortalgorithmes/IterativeSelectionSort.java
@@ -2,28 +2,31 @@ package algorithmes.sortalgorithmes;
 
 import java.util.Arrays;
 
-public class IterativeSelectionSort {
+public class IterativeSelectionSort
+{
 
-    public static void main(String[] args) {
-        int[] array = { 5, 89, 5, -4, 0, 56, 3, 2, 6, -9 };
+    public static void main(String[] args)
+    {
+        int[] array = {5, 89, 5, -4, 0, 56, 3, 2, 6, -9};
         selectionSort(array);
         System.out.println(Arrays.toString(array));
     }
 
-    private static void selectionSort(int[] numbers){
-        int min, temp;
-
-        for (int index = 0; index < numbers.length-1; index++){
-            min = index;
-            for (int scan = index+1; scan < numbers.length; scan++){
-                if (numbers[scan] < numbers[min])
-                    min = scan;
+    private static void selectionSort(int[] arr)
+    {
+        for (int min = 0; min < arr.length - 1; min++)
+        {
+            int least = min;
+            for (int j = min + 1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[least])
+                {
+                    least = j;
+                }
             }
-
-            // Swap the values
-            temp = numbers[min];
-            numbers[min] = numbers[index];
-            numbers[index] = temp;
+            int tmp = arr[min];
+            arr[min] = arr[least];
+            arr[least] = tmp;
         }
     }
 }
